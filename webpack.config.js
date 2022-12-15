e02cd1903d2c0ac219b5037076d3dead10a8075a
dyv21 <d.y.vladimirov@gmail.com>
2022-12-15 11:44:20 -0500

    Add and set webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = requ ire('path');
+
+module.exports = {
+  entry: './path/to/my/entry/file.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'my-first-webpack.bundle.js',
+  },
+};
